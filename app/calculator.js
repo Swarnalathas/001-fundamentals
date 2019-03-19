158cc814e2466448ac49bad81bc683f2156d4e45
Swarna <swarna.prakashm@gmail.com>
2019-03-19 21:54:56 +0000

    work commit day 1

--- app/calculator.js
+++ app/calculator.js
@@ -2,6 +2,44 @@ function add(a, b) {
     return a + b;
 }
 
+function Subtract(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b){
+    return a * b;
+}
+
+function isOdd(num){
+    var Result = num % 2;
+    if (Result === 1 ) return true;
+    if (Result === 0)  return false;
+}
+
+function isEven(num) {
+    var Result1 = num % 2;
+    if (Result1 === 1) return false;
+    if (Result1 === 0) return true;
+ }
+
+ function divisibleBy(a, b) {
+     var Result2 = a % b;
+     if (Result2 === 0) return true; 
+     if (Result2 === 1) return false;
+ }
+ 
+ function squareOf(num) {
+
+    var Result4 = num * num;
+
+    return Result4;
+ }
+
+ const myBeverage = {
+    delicious: true,
+    sour: false,
+  };
+
 module.exports = {
-    add
+    add, Subtract,Multiply,isOdd,isEven,divisibleBy,squareOf,myBeverage
 };
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,40 @@ const calculator = require('../app/calculator');
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
+
+test('subtract 4 - 2 to equal 2', () => {
+  expect(calculator.Subtract(4, 2)).toBe(2);
+});
+
+
+test('multifly 4 * 2 to equal 8', () => {
+  expect(calculator.Multiply(4, 2)).toBe(8);
+});
+
+test ('IsOdd funtion if Result is odd return true', () => {
+  expect(calculator.isOdd(5)).toBe(true);
+});
+
+test ('IsEven funtion if Result is Even return true', () => {
+  expect(calculator.isEven(6)).toBe(true);
+});
+
+test ('divisibleBy check the numbers ', () => {
+  expect(calculator.divisibleBy(10,5)).toBe(true);
+});
+
+test('Square of the given number',()  => {
+ expect(calculator.squareOf(10)).toBe(100);
+});
+
+
+
+describe('my beverage', () => {
+  test('is delicious', () => {
+    expect(calculator.myBeverage.delicious).toBeTruthy();
+  });
+
+  test('is not sour', () => {
+    expect(calculator.myBeverage.sour).toBeFalsy();
+  });
+});
